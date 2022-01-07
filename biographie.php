6a49b4a3df7c3e033e7d9c9e3ad9f66cd682ce50
Fat-Fat <trisson.valentin@gmail.com>
2022-01-07 15:31:30 +0100

    Added biographie

--- biographie.php
+++ biographie.php
@@ -16,7 +16,54 @@
     <?php include('header.html'); ?>
 
     <section id="section_body_biographie" class="content_secondary">
+    <div id="offset"></div>
+      <h2>Biographie</h2>
+      <br>
+      <h3>Formation et débuts (2009 - 2013)</h3>
+      <p>Le groupe se forme en 2009 à Columbus, dans l'Ohio, par trois camarades de lycée : Tyler Joseph (qui, en solo, a déjà sorti un album, No Phun Intended), Nick Thomas et Chris Salih1.
 
+        Tyler Joseph attribue au groupe son nom en s'inspirant de l'ouvrage All My Sons d'Arthur Miller qui parle d'un homme décidant du destin de sa famille après avoir tué 21 pilotes (d’où «Twenty One Pilots» ) durant la Seconde Guerre mondiale à cause de la livraison de pièces d'avion défectueuses. Il explique que la morale de l'histoire a inspiré le nom du groupe.
+
+        Le 29 décembre 2009, Twenty One Pilots offre au public un premier album, Twenty One Pilots, et se lance dans une tournée à travers l'Ohio. La même année sort Johnny Boy EP qui comprend 6 chansons dont Time to say goodbye.
+
+        Au milieu de l'année 2011, Nick Thomas et Chris Salih quittent le groupe par manque de temps. Joshua Dun, ancien batteur live de House of Heroes, les rejoint. Le deuxième album ou le mixtape, Regional at Best, sort le 8 juillet 2011, composé uniquement par Joseph et Dun. L'album s'accompagne d'un CD gratuit distribué au New Albany High School. En novembre 2011, ils jouent un concert à guichet fermé au Newport Music Hall de Columbus, attirant l'attention d'une douzaine de labels. Le groupe décide finalement de signer au label Fueled by Ramen d'Atlantic Records.</p>
+      <br>
+      <br>
+      <h3>Vessel et débuts télévisés (2012 - 2013/2013-2014)</h3>
+      <p>En avril 2012, lors d'un concert à guichet fermé au Lifestyle Communities Pavilion, ils annoncent leur signature avec le label Fueled by Ramen d'Atlantic Records. Le 17 juillet 2012, ils publient leur premier album chez Fueled by Ramen, un EP de trois chansons intitulé Three Songs. En août 2012, ils embarquent dans une brève tournée avec Neon Trees et Walk the Moon4. Ils collaborent avec Greg Wells, producteur d'Adele et Katy Perry, pour leur premier album studio, Vessel5,6. Il est publié le 8 janvier 2013 et atteint la 58e place du Billboard 200, la 42e place des Digital Albums, la 17e place des Internet Albums, la 15e place des Rock Albums, et la 10e des Alternative Albums7.
+
+      Le 12 novembre 2012, le groupe publie la vidéo de la chanson Holding On to You sur YouTube. En 2013, les clips de Guns for Hands et Car Radio sont publiés les 7 janvier et 19 avril, respectivement. En mai 2013, Fall Out Boy annonce tourner aux côtés de Twenty One Pilots au Save Rock and Roll Arena Tour8. Le 8 août 2013, Twenty One Pilots jouent House of Gold. Le 24 décembre 2013, pendant le réveillon de Noël, Tyler Joseph chante la chanson O come, O come, Emmanuel au Five14's Christmas With the Stars de New Albany, dans l'Ohio. Le clip de la chanson est publié sur YouTube le 14 février 2014. Le 31 décembre, le clip de la chanson Truce est publié sur YouTube.</p>
+      <br>
+      <br>
+      <h3>Montée et Quiet Is Violent Tour (2014)</h3>
+      <p>En avril 2012, lors d'un concert à guichet fermé au Lifestyle Communities Pavilion, ils annoncent leur signature avec le label Fueled by Ramen d'Atlantic Records. Le 17 juillet 2012, ils publient leur premier album chez Fueled by Ramen, un EP de trois chansons intitulé Three Songs. En août 2012, ils embarquent dans une brève tournée avec Neon Trees et Walk the Moon. Ils collaborent avec Greg Wells, producteur d'Adele et Katy Perry, pour leur premier album studio, Vessel5,6. Il est publié le 8 janvier 2013 et atteint la 58e place du Billboard 200, la 42e place des Digital Albums, la 17e place des Internet Albums, la 15e place des Rock Albums, et la 10e des Alternative Albums.
+
+      Le 12 novembre 2012, le groupe publie la vidéo de la chanson Holding On to You sur YouTube. En 2013, les clips de Guns for Hands et Car Radio sont publiés les 7 janvier et 19 avril, respectivement. En mai 2013, Fall Out Boy annonce tourner aux côtés de Twenty One Pilots au Save Rock and Roll Arena Tour8. Le 8 août 2013, Twenty One Pilots jouent House of Gold. Le 24 décembre 2013, pendant le réveillon de Noël, Tyler Joseph chante la chanson O come, O come, Emmanuel au Five14's Christmas With the Stars de New Albany, dans l'Ohio. Le clip de la chanson est publié sur YouTube le 14 février 2014. Le 31 décembre, le clip de la chanson Truce est publié sur YouTube.</p>
+      <br>
+      <br>
+      <h3>Blurryface (2015 - 2018)</h3>
+      <p>Le 16 mars 2015, le groupe annonce la sortie de Blurryface pour le 19 mai. Ils publient aussi leur premier single, Fairly Local. Leur second single de l'album, Tear in My Heart, est publié le 5 avril 2015. Le 28 avril, Stressed Out, le troisième single de l'album, est publié. Le clip atteint plus de 2 milliards de vues sur YouTube.
+
+Blurryface est finalement publié quatre jours avant sa sortie officielle, le 15 mai 2015.L'album compte 14 chansons. Il s'écoule à 134 000 exemplaires la première semaine et plus de 300 000 par la suite. Cet album atteint la première place du Billboard 200.
+
+Le groupe lance la tournée mondiale le « Blurryface World Tour » le 11 mai 2015 à Glasgow, en Écosse. La tournée passe par les États-Unis, le Canada, l'Australie, l'Asie du sud-est, le Japon et l'Europe23. Le 9 mai 2016, le groupe annonce sur sa page Facebook une deuxième prolongation de sa tournée mondiale, après en avoir effectuée une première en novembre 2015.
+
+En 2016, le groupe sort le single Heathens, pour le film Suicide Squad et l'album du même nom. La même année, ils publient le titre Cancer, qui reprend le titre de même nom du groupe My Chemical Romance.
+
+Le 3 février 2017, le groupe publie le clip vidéo de Heavydirtysoul. Le 5 et 7 février 2017, le groupe annonce la fin de l'ère Blurryface avec le dernier épisode de leur série Sleepers Chapter sur YouTube, ainsi qu'un mystérieux événement crypté sur les réseaux sociaux Twitter et Facebook.</p>
+    </section>
+      <br>
+      <br>
+      <h3>Trench (2018 - 2021)</h3>
+      <p>Le 11 juillet 2018, le groupe poste sur YouTube le clip vidéo de la chanson Jumpsuit et une version audio de Nico and The Niners. C'est la première manifestation du groupe après plus d'un an d'interruption, pause remise en cause par le chanteur lui-même dans le clip Jumpsuit « We've been here the whole time. You were asleep, time to wake up. ». Dans la foulée, le groupe annonce la sortie de son cinquième album studio Trench prévue le 5 octobre 2018 et le « The Bandito Tour »: une tournée mondiale. Ce nouvel album confirme le succès du groupe. En septembre 2018, ils sont acclamés par le public lorsqu'ils font leur grand retour sur scène dans la célèbre salle de concert de Londres: la Carling Brixton Academy.
+
+En 2020, pendant le confinement, le groupe publie une chanson sur la quarantaine, intitulée Level of Concern. Il lance par ailleurs sur YouTube le Never-Ending Music Video, une diffusion du titre lancée depuis le 22 juin 2020. Ce clip a été accompagné d'un nouveau site27 web. Celle-ci se termine le 17 décembre après une vidéo humoristique du batteur du groupe, dans laquelle il débranche accidentellement le serveur qui gardait le direct actif pour alimenter son sapin de noël. La vidéo est devenue le clip vidéo le plus long de l'histoire et la vidéo la plus longue de la plateforme YouTube bien que sa rediffusion ne soit disponible. La même année, le groupe sort un single de Noël, le 8 décembre, Christmas Saves the Year qui parle de noël pendant la pandémie.</p>
+      <br>
+      <br>
+      <h3>Scaled and Icy (2021 -)</h3>
+      <p>Le sixième album studio du duo, Scaled and Icy, est sorti le 21 mai 2021. Le nom de l'album est une anagramme, qui énonce "Clancy est mort", et un mélange avec l'expression "réduite et isolée", écrite par Tyler Joseph. Le 4 mars 2019, cinq mois après la sortie de leur cinquième album studio Trench, le groupe a confirmé qu'ils travaillaient sur leur prochain album studio. À propos du thème possible, Joseph a déclaré: "Il y a un personnage dont on n'a encore parlé sur aucun disque qui joue un rôle énorme dans le récit dont il faudra évidemment parler et c'est probablement là où nous allons ensuite".
+
+En novembre 2020, Josh Dun a révélé qu'ils travaillaient toujours "à distance" sur l'album, les deux membres se trouvant dans des endroits différents. Il décrit le processus d'enregistrement : « nous avons tous les deux nos propres studios, ce qui est vraiment bien, alors il propose beaucoup de trucs dans son studio, me les envoie, puis je propose des trucs ici dans mon studio puis je lui renvoie". Ils ont commencé à teaser la nouvelle ère du groupe en janvier 2021. L'album a été annoncé pour la première fois le 6 avril 2021 via dmaorg.info, un site Web que le groupe utilise pour partager ses nouveautés, via une affiche comprenant le texte "New Album and Livestream Experience". L'album a été officiellement annoncé par le groupe un jour plus tard. Le premier single "Shy Away" est sorti le même jour, accompagné d'un clip. Le deuxième single de l'album, "Choker", est sorti le 30 avril 2021, aux côtés d'un clip. Le troisième single de l'album, "Saturday", est sorti le 18 mai 2021. Le groupe a annoncé un concert en direct quelques jours avant la sortie de "Shy Away", qui a eu lieu le 21 mai 2021, coïncidant avec la sortie de l'album. Le livestream, décrit comme « couvrant toute la carrière », comprenait des performances en direct de chansons de Scaled and Icy, ainsi que des albums précédents. L'achat d'un billet pour le livestream a fourni aux fans une expérience de pré-spectacle de six semaines avec un accès exclusif à des variantes de marchandises et d'albums. Dans une vidéo promotionnelle, Josh Dun a comparé l'accès avant le spectacle à l'expérience comme faire la queue avec d'autres fans avant le début d'un concert.</p>
     </section>
 
     <?php include('footer.html'); ?>
--- discographie.php
+++ discographie.php
@@ -21,42 +21,44 @@
             <img src="Ressources/album1.png" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Twenty One Pilots</i> (2009)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>Twenty One Pilots est le premier album studio éponyme du groupe américain Twenty One Pilots, sorti le 29 décembre 2009. L'album s'est vendu à 115 000 exemplaires et a culminé à la 139e place sur le Billboard 200 américain. C'est le seul album à présenter le bassiste Nick Thomas et le batteur Chris Salih avant qu'ils ne quittent le groupe en 2011. Avant cet album, le groupe a sorti le Johnny Boy EP ainsi que l'album solo de tyler No Phun Intended</p>
             </div>
         </section>
         <section>
             <img src="Ressources/album2.jpg" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Regional at best</i> (2011)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>La sortie de l'album date du 8 juillet 2011. C'est le premier album avec Joshua Dun, le batteur du duo, après le départ du bassiste Nick Thomas et du batteur Chris Salih en 2011. C'est également le dernier album avant le label Fueled by Ramen.</p>
             </div>
         </section>
         <section>
             <img src="Ressources/album3.jpg" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Vessel</i> (2013)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>Vessel est le troisième album du groupe Twenty One Pilots, sorti le 8 janvier 2013. Durant l'été 2012, Twenty One Pilots sort l'EP Three Songs. Holding On To You paraît en single le 11 septembre. L'album est disponible en streaming le 18 décembre et est finalement publié le 8 janvier 2013. Les deux hommes sur la pochette de l'album sont les grands-pères des deux membres du groupe. La photo avait été choisie pour voir la réaction de leur label.</p>
             </div>
         </section>
         <section>
             <img src="Ressources/album4.jpg" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Blurryface</i> (2015)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>Le 16 mars 2015, le groupe annonce la sortie de Blurryface pour le 19 mai. Ils publient aussi leur premier single, Fairly Local. Leur second single de l'album, Tear in My Heart, est publié le 5 avril 2015. Le 28 avril, Stressed Out, le troisième single de l'album, est publié. Le clip atteint plus de 2 milliards de vues sur YouTube.</p>
             </div>
         </section>
         <section>
             <img src="Ressources/album5.jpg" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Trench</i> (2018)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>Trench est le cinquième album studio du duo musical américain Twenty One Pilots, sorti le 5 octobre 2018 avec le label Fueled by Ramen.
+
+                Il s'agit du premier album studio du groupe en trois ans, après le succès décisif de leur précédent album, Blurryface sorti en 2015. Il fait en fait suite à ce dernier, en poursuivant la narration tout en élargissant l’univers fictif créé autour des deux protagonistes. Enregistré en secret accompagné d'un silence radio d'un an, Trench a été conçu pour être un album concept qui explore les sujets de la santé mentale, du suicide et du doute.</p>
             </div>
         </section>
         <section>
             <img src="Ressources/album6.jpg" alt="Couverture de pochette">
             <div class="content_text">
                 <h2><i>Scaled and Icy</i> (2021)</h2>
-                <p>shfuhfuyzhefhuih o ijdizjhfui haojosidjiuyhuiyheuifn</p>
+                <p>Scaled and Icy est le sixième album du duo américain Twenty One Pilots, sorti le 21 mai 2021 chez Fueled by Ramen et Elektra. Le titre de l'album est une anagramme de « Clancy is dead », une référence au protagoniste du dernier album Trench.</p>
             </div>
         </section>
         
