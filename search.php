f87d5fb5520844f2f0e122e5b820a79bb7959271
Fat-Fat <trisson.valentin@gmail.com>
2022-01-07 21:00:28 +0100

    Improved search

--- search.php
+++ search.php
@@ -1,58 +1,64 @@
 <?php
-//Attente de l'appui du bouton pour lancer la fonction isInputInFile()
-if(array_key_exists('button_search',$_POST)){
-    isinputInFile();
-}
 
 //Initialisation d'un tableau qui contiendra la liste des musiques
 $musicListTab = [];
 
 //Ouverture du fichier list_music.txt en lecture uniquement.
-$file = fopen("list_music.txt", "r") or die("Liste de musique introuvable !"); //Ouverture du fichier
+$fileSong = fopen("list_music.txt", "r") or die("Liste de musique introuvable !"); //Ouverture du fichier
 
-// On entre toute les valeurs lignes par lignes dans le tab
-while(!feof($file)) {
-  $musicListTab[] =  fgets($file);
+//Boucle while() faisant x nombre d'itérations avec x étant le nombre de ligne du fichier.
+//Chaque ligne du fichier deviens une valeur du tableau musicListTab[].
+while(!feof($fileSong)) {
+  $musicListTab[] =  fgets($fileSong);
   $sizeTab = count($musicListTab);
 }
 
-//DEBUG - AFFICHAGE DU TABLEAU
-$i = 0;
-while($i< $sizeTab) {
-    echo ($musicListTab[$i]) . "<br>";
-    $i = $i + 1;
-}
+//DEBUG ONLY
+//Affichage des musiques existantes d'après le tableau.
+// $i = 0;
+// while($i< $sizeTab) {
+//     echo ($musicListTab[$i]) . "<br>";
+//     $i = $i + 1;
+// }
 
 
 
-
+//MAIN FUNCTION
 function isinputInFile()
 {
-    
-   $valueInput = $_POST["search_field"];
-   //DEBUG Affichage de la valeur
-    // echo "<script>alert('$valueInput');</script>";
+    //On récupére la valeur de champ de texte
+    $valueInput = $_POST["search_field"];
+    $valueInputUppercase = strtoupper($valueInput);
 
-    $i = 0;
+    //On récupére les valeurs initié en dehors de cette fonction
     global $sizeTab;
     global $musicListTab;
-    global $valueInput;
-    echo "<script>alert('$sizeTab');</script>";
-    while($i<$sizeTab) {
-        if (str_contains($musicListTab[$i], $valueInput)) { 
-            echo 'COUCOU';
-        }
-        else {
-            echo 'seeya';
-        }
-        echo($musicListTab[0]);
-        echo($sizeTab);
-        echo($valueInput);
 
+    //DEBUG ONLY
+    //Affichage de la valeur récupérée
+    // echo "<script>alert('$valueInput');</script>";
+    //Affichage de la taille du tableau récupéré
+    // echo "<script>alert('$sizeTab');</script>";
+
+    $i = 0;
+    $resultNumber = 0;
+    while($i<$sizeTab) {
+        $musicListTabUppercase = strtoupper($musicListTab[$i]);
+        if (str_contains($musicListTabUppercase, $valueInputUppercase)) { 
+            $resultNumber = $resultNumber + 1;
+            echo ($musicListTab[$i]).' contient la recherche "' . ($valueInput) . '". <br>';
+        }
         $i = $i + 1;
       }
+    if ($resultNumber == 0) {
+        echo "Aucun résultat trouvé";
+    }
 }
 
+//Attente de l'appui du bouton pour lancer la fonction isInputInFile()
+if(array_key_exists('button_search',$_POST)){
+    isinputInFile();
+}
 
-fclose($file);
+fclose($fileSong);
 ?>
