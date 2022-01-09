f3dd5a981c4f9307362285b5269e5139c83b0028
Fat-Fat <trisson.valentin@gmail.com>
2022-01-09 21:45:06 +0100

    Updates

--- add.php
+++ add.php
@@ -1,66 +1,24 @@
 <?php
 
-$sizeTab = 0;
-$sizeTabUrl = 0;
-$sizeTabDuration = 0;
-$sizeTabAlbum = 0;
+//Initialisation de variables
+$sizeTab = $sizeTabUrl = $sizeTabDuration = $sizeTabAlbum = $i = 0;
+$musicListTab = $urlListTab = $durationListTab = $albumListTab = [];
+global $musicListTab;
+global $urlListTab;
+global $durationListTab;
+global $albumListTab;
 
-$musicListTab = [];
-$urlListTab = [];
-$durationListTab = [];
-$albumListTab = [];
-
-function fileToTabSong() {
-    global $sizeTab, $musicListTab;
-    //Ouverture du fichier list_music.txt en lecture uniquement.
-    $fileSong = fopen("list_music.txt", "r") or die("Liste de musique introuvable !");
-    //Boucle while() faisant x nombre d'itérations avec x étant le nombre de ligne du fichier.
-    //Chaque ligne du fichier deviens une valeur du tableau musicListTab[].
-    while(!feof($fileSong)) {
-        $musicListTab[] =  fgets($fileSong);
-        $sizeTab = count($musicListTab);
-    }
-    fclose($fileSong);
-}
-function fileToTabURL() {
-    global $sizeTabUrl, $urlListTab;
-
-    $fileUrl = fopen("list_url.txt", "r") or die("Liste des URLs introuvable !");
-    while(!feof($fileUrl)) {
-        $urlListTab[] =  fgets($fileUrl);
-        $sizeTabUrl = count($urlListTab);
-    }
-    fclose($fileUrl);
-}
-function fileToTabDuration() {
-    global $sizeTabDuration, $durationListTab;
-
-    $fileDuration = fopen("list_duration.txt", "r") or die("Liste de dates de parutions introuvable !");
-    while(!feof($fileDuration)) {
-        $durationListTab[] =  fgets($fileDuration);
-        $sizeTabDuration = count($durationListTab);
-    }
-    fclose($fileDuration);
-}
-function fileToTabAlbum() {
-    global $sizeTabAlbum, $albumListTab;
-
-    $fileAlbum = fopen("list_album.txt", "r") or die("Liste des albums introuvable !");
-    while(!feof($fileAlbum)) {
-        $albumListTab[] =  fgets($fileAlbum);
-        $sizeTabAlbum = count($albumListTab);
-    }
-    fclose($fileAlbum);
-}
 fileToTabSong();
 fileToTabURL();
 fileToTabDuration();
 fileToTabAlbum();
+
+//Vérification de la taille des tableau pour éviter toute corruption de données
 if ((($sizeTab != $sizeTabUrl)||($sizeTabDuration != $sizeTabAlbum))||($sizeTab != $sizeTabAlbum)) {
     echo "<script>alert('Erreur critique : certains fichiers contenant les données possédent des tailles différentes.');</script>";
     echo "L'affichage des résultats peut être défectueux.";
 }
-$i=0;
+
 while($i<$sizeTab) {
     displayInfo($i);
     $i = $i + 1;
@@ -103,6 +61,51 @@ function addDataToFile($title,$duration,$album,$url){
     fclose($albumFile);
     fclose($urlFile);
 
+}
+
+//Fonctions convertissant les fichiers en tableau
+function fileToTabSong() {
+    global $sizeTab, $musicListTab;
+    //Ouverture du fichier list_music.txt en lecture uniquement.
+    $fileSong = fopen("Datafiles/list_music.txt", "r") or die("Liste de musique introuvable !");
+    //Boucle while() faisant x nombre d'itérations avec x étant le nombre de ligne du fichier.
+    //Chaque ligne du fichier deviens une valeur du tableau musicListTab[].
+    while(!feof($fileSong)) {
+        $musicListTab[] =  fgets($fileSong);
+        $sizeTab = count($musicListTab);
+    }
+    //Fermeture du fichier
+    fclose($fileSong);
+}
+function fileToTabURL() {
+    global $sizeTabUrl, $urlListTab;
+
+    $fileUrl = fopen("Datafiles/list_url.txt", "r") or die("Liste des URLs introuvable !");
+    while(!feof($fileUrl)) {
+        $urlListTab[] =  fgets($fileUrl);
+        $sizeTabUrl = count($urlListTab);
+    }
+    fclose($fileUrl);
+}
+function fileToTabDuration() {
+    global $sizeTabDuration, $durationListTab;
+
+    $fileDuration = fopen("Datafiles/list_duration.txt", "r") or die("Liste de dates de parutions introuvable !");
+    while(!feof($fileDuration)) {
+        $durationListTab[] =  fgets($fileDuration);
+        $sizeTabDuration = count($durationListTab);
+    }
+    fclose($fileDuration);
+}
+function fileToTabAlbum() {
+    global $sizeTabAlbum, $albumListTab;
+
+    $fileAlbum = fopen("Datafiles/list_album.txt", "r") or die("Liste des albums introuvable !");
+    while(!feof($fileAlbum)) {
+        $albumListTab[] =  fgets($fileAlbum);
+        $sizeTabAlbum = count($albumListTab);
+    }
+    fclose($fileAlbum);
 }
 
 
--- search.php
+++ search.php
@@ -1,62 +1,18 @@
 <?php
 
-$sizeTab = 0;
-$sizeTabUrl = 0;
-$sizeTabDuration = 0;
-$sizeTabAlbum = 0;
+//Initialisation de variables
+$sizeTab = $sizeTabUrl = $sizeTabDuration = $sizeTabAlbum = $i = 0;
+$musicListTab = $urlListTab = $durationListTab = $albumListTab = [];
+$prefixDev = '@';
 
-$musicListTab = [];
-$urlListTab = [];
-$durationListTab = [];
-$albumListTab = [];
+//Fonctions convertissant les fichiers en tableau
 
-function fileToTabSong() {
-    global $sizeTab, $musicListTab;
-    //Ouverture du fichier list_music.txt en lecture uniquement.
-    $fileSong = fopen("list_music.txt", "r") or die("Liste de musique introuvable !");
-    //Boucle while() faisant x nombre d'itérations avec x étant le nombre de ligne du fichier.
-    //Chaque ligne du fichier deviens une valeur du tableau musicListTab[].
-    while(!feof($fileSong)) {
-        $musicListTab[] =  fgets($fileSong);
-        $sizeTab = count($musicListTab);
-    }
-    fclose($fileSong);
-}
-function fileToTabURL() {
-    global $sizeTabUrl, $urlListTab;
-
-    $fileUrl = fopen("list_url.txt", "r") or die("Liste des URLs introuvable !");
-    while(!feof($fileUrl)) {
-        $urlListTab[] =  fgets($fileUrl);
-        $sizeTabUrl = count($urlListTab);
-    }
-    fclose($fileUrl);
-}
-function fileToTabDuration() {
-    global $sizeTabDuration, $durationListTab;
-
-    $fileDuration = fopen("list_duration.txt", "r") or die("Liste de dates de parutions introuvable !");
-    while(!feof($fileDuration)) {
-        $durationListTab[] =  fgets($fileDuration);
-        $sizeTabDuration = count($durationListTab);
-    }
-    fclose($fileDuration);
-}
-function fileToTabAlbum() {
-    global $sizeTabAlbum, $albumListTab;
-
-    $fileAlbum = fopen("list_album.txt", "r") or die("Liste des albums introuvable !");
-    while(!feof($fileAlbum)) {
-        $albumListTab[] =  fgets($fileAlbum);
-        $sizeTabAlbum = count($albumListTab);
-    }
-    fclose($fileAlbum);
-}
 
 fileToTabSong();
 fileToTabURL();
 fileToTabDuration();
 fileToTabAlbum();
+//Vérification de la taille des tableau pour éviter toute corruption de données
 if ((($sizeTab != $sizeTabUrl)||($sizeTabDuration != $sizeTabAlbum))||($sizeTab != $sizeTabAlbum)) {
     echo "<script>alert('Erreur critique : certains fichiers contenant les données possédent des tailles différentes.');</script>";
     echo "L'affichage des résultats peut être défectueux.";
@@ -75,14 +31,57 @@ function isinputInFile()
     global $sizeTab;
     global $musicListTab;
     global $urlListTab;
+    global $prefixDev;
 
     $i = 0;
     $resultNumber = 0;
 
     //Commande DEV avec @xx 
     //xx etant une valeur numérique entre 0 et 66
-    if (str_starts_with($valueInput, '@')){
+    if (str_starts_with($valueInput, $prefixDev)){
         $command = substr($valueInput, 1);
+        if(str_starts_with($command, 'show')){
+            $command = substr($command, 4);
+            if (is_numeric($command)){
+                if ($command>($sizeTab-1)){
+                    echo '<script>alert("Veuillez entrer une valeur inférieure à la quantité de musique.")</script>';
+                }else {
+                displayInfo($command);
+                }
+            }else {
+                echo '<script>alert("Veuillez entrer une valeur numérique uniquement.")</script>';
+            }
+        }
+        
+        if(str_starts_with($command, 'edit')){
+            $command = substr($command, 4);
+            if (is_numeric($command)){
+                if ($command>($sizeTab-1)){
+                    echo '<script>alert("Veuillez entrer une valeur inférieure à la quantité de musique.")</script>';
+                }else {
+                displayInfo($command);
+                }
+            }else {
+                echo '<script>alert("Veuillez entrer une valeur numérique uniquement.")</script>';
+            }
+        }
+
+        if(str_starts_with($command, 'remove')){
+            $command = substr($command, 6);
+            if (is_numeric($command)){
+                if ($command>($sizeTab-1)){
+                    echo '<script>alert("Veuillez entrer une valeur inférieure à la quantité de musique.")</script>';
+                }else {
+                displayInfo($command);
+                }
+            }else {
+                echo '<script>alert("Veuillez entrer une valeur numérique uniquement.")</script>';
+            }
+        }
+
+
+
+
         if($command != "") {
             if (is_numeric($command)){
                 if ($command>($sizeTab-1)){
@@ -116,16 +115,64 @@ function isinputInFile()
                 echo "0 résultats correspondant à votre recherche.";
             }
         }
+}
+
+//Fonctions convertissant les fichiers en tableau
+function fileToTabSong() {
+    global $sizeTab, $musicListTab;
+    //Ouverture du fichier list_music.txt en lecture uniquement.
+    $fileSong = fopen("Datafiles/list_music.txt", "r") or die("Liste de musique introuvable !");
+    //Boucle while() faisant x nombre d'itérations avec x étant le nombre de ligne du fichier.
+    //Chaque ligne du fichier deviens une valeur du tableau musicListTab[].
+    while(!feof($fileSong)) {
+        $musicListTab[] =  fgets($fileSong);
+        $sizeTab = count($musicListTab);
     }
+    //Fermeture du fichier
+    fclose($fileSong);
+}
+function fileToTabURL() {
+    global $sizeTabUrl, $urlListTab;
+
+    $fileUrl = fopen("Datafiles/list_url.txt", "r") or die("Liste des URLs introuvable !");
+    while(!feof($fileUrl)) {
+        $urlListTab[] =  fgets($fileUrl);
+        $sizeTabUrl = count($urlListTab);
+    }
+    fclose($fileUrl);
+}
+function fileToTabDuration() {
+    global $sizeTabDuration, $durationListTab;
+
+    $fileDuration = fopen("Datafiles/list_duration.txt", "r") or die("Liste de dates de parutions introuvable !");
+    while(!feof($fileDuration)) {
+        $durationListTab[] =  fgets($fileDuration);
+        $sizeTabDuration = count($durationListTab);
+    }
+    fclose($fileDuration);
+}
+function fileToTabAlbum() {
+    global $sizeTabAlbum, $albumListTab;
+
+    $fileAlbum = fopen("Datafiles/list_album.txt", "r") or die("Liste des albums introuvable !");
+    while(!feof($fileAlbum)) {
+        $albumListTab[] =  fgets($fileAlbum);
+        $sizeTabAlbum = count($albumListTab);
+    }
+    fclose($fileAlbum);
+}
+
 
 function displayInfo($index){
-global $musicListTab;
-global $urlListTab;
-global $durationListTab;
-global $albumListTab;
+    //On récupére les variables modifiées dans des fonctions hors de portée
+    global $musicListTab;
+    global $urlListTab;
+    global $durationListTab;
+    global $albumListTab;
 
-echo '<div class="result_search"><p class="title_search"> <strong>'.($musicListTab[$index]).'</strong> - '.($albumListTab[$index]).'<br>'.($urlListTab[$index]).'<br><p>Durée du titre : '.($durationListTab[$index]).'</p><br></div>';
+    echo '<div class="result_search"><p class="title_search"> <strong>'.($musicListTab[$index]).'</strong> - '.($albumListTab[$index]).'<br>'.($urlListTab[$index]).'<br><p>Durée du titre : '.($durationListTab[$index]).'</p><br></div>';
 }
+
 //Attente de l'appui du bouton pour lancer la fonction isInputInFile()
 if(array_key_exists('button_search',$_POST)){
     isinputInFile();
